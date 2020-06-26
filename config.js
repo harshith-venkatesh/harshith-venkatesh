a05dfaa415402a3f7cebac8815d27d5f898aa90e
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-26 05:55:46 +0530

    update

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  //pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Harshith',
   lastName: 'Venkatesh',
   // social
